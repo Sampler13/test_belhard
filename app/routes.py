4d022f9e30d9fb8a266ca8692ec2a965c6d47ade
Ilya Sharov <wmbeeman@gmail.com>
2025-09-30 23:15:12 +0300

    init

--- app/routes.py
+++ app/routes.py
@@ -43,7 +43,10 @@ def login():
             session['user_name'] = name
             session['user_surname'] = surname
             print("Session after login:", session)
-            return redirect(url_for('routes.index'))
+            print("Form data:", form)
+            print("User found:", user)
+            print("Password check result:", check_password_hash(user['password_hash'], password))
+            print("Session before redirect:", session)
         else:
             errors['login'].append("Неверный логин или пароль.")  #
 
